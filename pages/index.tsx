8f6b0b6bab388b56c4568d0d5429eb14965a7f5b
Pavol Novotny <pavol.novotny@drmax.cz>
2022-01-08 16:33:29 +0100

    fetching and structure

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,22 @@
-export default function Home() {
-  const message: string = 'Hello World'
+import type {InferGetStaticPropsType} from "next";
+import getAllProducts from "@framework/product/get-all-products";
+
+export default function Home({products}: InferGetStaticPropsType<typeof getStaticProps>) {
+
   return (
     <div>
-      {message}
+      {JSON.stringify(products)}
     </div>
   )
+}
+
+export async function getStaticProps() {
+  const products = await getAllProducts()
+
+  return {
+    props: {
+       products
+    },
+    revalidate: 4*60*60
+  }
 }

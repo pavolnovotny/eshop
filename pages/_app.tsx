b3dbb2884e8d7e69c72e6aa824cb73f72c21d1f1
Pavol Novotny <pavol.novotny@drmax.cz>
2022-01-09 17:03:00 +0100

    css tailwind

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,8 @@
 import {AppProps} from "next/app";
 import {FC} from "react";
+import "@assets/main.css"
 
 const Noop: FC = ({children}) => <>{children}</>
-
 
 function MyApp({Component, pageProps}: AppProps & {Component: {Layout: FC}}) {
   const Layout = Component.Layout ?? Noop

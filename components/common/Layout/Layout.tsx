532832f4228a47ab81343d1925a22f00d22c0a22
Pavol Novotny <pavol.novotny@drmax.cz>
2022-01-13 19:42:36 +0100

    ui provider

--- components/common/Layout/Layout.tsx
+++ components/common/Layout/Layout.tsx
@@ -1,11 +1,21 @@
-import {FC, Fragment} from 'react'
+import {FC} from 'react'
 import style from './Layout.module.css'
 import {Footer, Navbar} from "@components/common";
+import { Sidebar } from "@components/ui"
+import { CartSidebar } from "@components/cart"
+import {useUI} from "@components/ui/context";
 
 const Layout: FC = ({children}) => {
+  const {isSidebarOpen, closeSidebar} = useUI()
+
   return (
     <div className={style.root}>
       <Navbar/>
+      <Sidebar
+        onClose={closeSidebar}
+        isOpen={isSidebarOpen}>
+        <CartSidebar />
+      </Sidebar>
       <main className="fit">
         { children }
       </main>
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,15 +1,18 @@
 import {AppProps} from "next/app";
 import {FC} from "react";
 import "@assets/main.css"
+import {UIProvider} from "@components/ui/context";
 
 const Noop: FC = ({children}) => <>{children}</>
 
 function MyApp({Component, pageProps}: AppProps & {Component: {Layout: FC}}) {
   const Layout = Component.Layout ?? Noop
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <UIProvider>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </UIProvider>
   )
 }
 export default MyApp
